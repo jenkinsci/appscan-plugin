cd752b3d304f4c6828d0e9cf2c8735813704c374
hjoshua <hjoshua@ca.ibm.com>
2017-06-15 15:36:51 -0400

    clean up

--- trn.sh
+++
@@ -1,5 +0,0 @@
-rm -Rf messages
-find . -name "*en.properties" -exec ditto {} messages/{} \;
-find . -name "*essages.properties" -exec ditto {} messages/{} \;
-find messages -name "*.properties" -exec perl -pi.bak -e 's/\r\n|\n|\r/\r\n/g' {} \;
-find . -name "*.bak" -type f -exec rm {} \;
