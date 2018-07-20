336c57b9ee23904626299202704ef67460ab0ee7
anurag <anurag-s@hcl.com>
2018-07-20 16:49:48 +0530

    updating the copyright

--- src/main/java/com/ibm/appscan/jenkins/plugin/builders/AppScanBuildStep.java
+++ src/main/java/com/ibm/appscan/jenkins/plugin/builders/AppScanBuildStep.java
@@ -1,6 +1,6 @@
 /**
  * @ Copyright IBM Corporation 2016.
- * @ Copyright HCL Technologies Ltd. 2017.
+ * @ Copyright HCL Technologies Ltd. 2017,2018.
  * LICENSE: Apache License, Version 2.0 https://www.apache.org/licenses/LICENSE-2.0
  */
 
