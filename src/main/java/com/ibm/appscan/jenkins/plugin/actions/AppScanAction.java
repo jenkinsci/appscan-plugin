e225e97077019f22c1c3dd1cd24443d77053d599
Vinita Sanghi <vinita-s@hcl.com>
2019-05-07 16:57:06 +0530

    IBM Domain Change

--- src/main/java/com/ibm/appscan/jenkins/plugin/actions/AppScanAction.java
+++ src/main/java/com/ibm/appscan/jenkins/plugin/actions/AppScanAction.java
@@ -1,5 +1,6 @@
 /**
  * © Copyright IBM Corporation 2016.
+ * @ Copyright HCL Technologies Ltd. 2019.
  * LICENSE: Apache License, Version 2.0 https://www.apache.org/licenses/LICENSE-2.0
  */
 
@@ -11,7 +12,7 @@ import hudson.model.Job;
 public abstract class AppScanAction implements Action {
 
 	protected static final String ICON = "/plugin/ibm-application-security/images/ASoC.ico"; //$NON-NLS-1$
-	protected static final String URL = "https://appscan.ibmcloud.com"; //$NON-NLS-1$
+	protected static final String URL = "https://cloud.appscan.com"; //$NON-NLS-1$
 	protected final Job<?,?> m_project;
 	
 	public AppScanAction (Job<?,?> project) {
