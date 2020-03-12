e855a87014b1d8056eb5b8b9a94500d9c906969f
Matthew Murphy <murphy@mmurphy.local>
2020-03-12 13:35:12 -0400

    remove all code from deprecated plugin

--- src/main/java/com/hcl/appscan/jenkins/plugin/scanners/ScanDescriptor.java
+++
@@ -1,8 +0,0 @@
-package com.hcl.appscan.jenkins.plugin.scanners;
-
-import hudson.model.Descriptor;
-
-public abstract class ScanDescriptor extends Descriptor<Scanner> {
-	public ScanDescriptor() {
-	}
-}
