5ad62e0593e618f282d5ac8b56ffdc4a5dbbd15d
Tanmoy Guha <tanmoy.guha@hcl.com>
2020-03-27 17:09:41 +0530

    ASA-2021 Comments Updated

--- src/main/java/com/hcl/appscan/jenkins/plugin/util/ExecutorUtil.java
+++ src/main/java/com/hcl/appscan/jenkins/plugin/util/ExecutorUtil.java
@@ -1,3 +1,8 @@
+/**
+ * © Copyright HCL Technologies Ltd. 2017, 2019.
+ * LICENSE: Apache License, Version 2.0 https://www.apache.org/licenses/LICENSE-2.0
+ */
+
 package com.hcl.appscan.jenkins.plugin.util;
 
 import java.util.concurrent.Callable;
