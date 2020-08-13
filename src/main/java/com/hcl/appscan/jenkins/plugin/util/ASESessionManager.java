f1277ac95319393f1b7cdc93915f9edaa06a0589
tanmoyguha-hcl <59166180+tanmoyguha-hcl@users.noreply.github.com>
2020-08-13 18:04:44 +0530

    ASA-2477 Added Copyright Statements and Updated ChangeLog File (#102)
    * Synchronization Issue Resolved
    
    * Pom File Updated to support Testing
    
    * Indentation Updated
    
    * ASA-2477
    
    * CHANGELOG Updated
    
    * CHANGELOG Updated
    
    * Updated SDK  Version in POM
    
    * ASA-2477 Reusing Credential Object to restrict Creation of multiple ASE Session
    
    * Copyright Statements Updated and ChangeLog Updated
    
    Co-authored-by: tanmoyguha-hcl <tanmoyguha-hcl@users.noreply.github.com>

--- src/main/java/com/hcl/appscan/jenkins/plugin/util/ASESessionManager.java
+++ src/main/java/com/hcl/appscan/jenkins/plugin/util/ASESessionManager.java
@@ -1,3 +1,8 @@
+/**
+ * @ Copyright HCL Technologies Ltd. 2020.
+ * LICENSE: Apache License, Version 2.0 https://www.apache.org/licenses/LICENSE-2.0
+ */
+
 package com.hcl.appscan.jenkins.plugin.util;
 
 import com.hcl.appscan.jenkins.plugin.auth.ASECredentials;
