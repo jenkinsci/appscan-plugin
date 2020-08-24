30a25d8d16515da0129c342233cd8461d5ee51bf
tanmoyguha-hcl <tanmoyguha-hcl@users.noreply.github.com>
2020-08-24 16:20:28 +0530

    Code Realigned

--- src/main/java/com/hcl/appscan/jenkins/plugin/actions/ScanResults.java
+++ src/main/java/com/hcl/appscan/jenkins/plugin/actions/ScanResults.java
@@ -1,6 +1,6 @@
 /**
  * © Copyright IBM Corporation 2016.
- * @ Copyright HCL Technologies Ltd. 2019.
+ * @ Copyright HCL Technologies Ltd. 2019, 2020.
  * LICENSE: Apache License, Version 2.0 https://www.apache.org/licenses/LICENSE-2.0
  */
 
