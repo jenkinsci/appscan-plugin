6312e2806526051e8f5110552e6651af736dd9d4
Kripajoy Melitpalathingal <36063366+kripajoym@users.noreply.github.com>
2022-04-14 13:11:03 +0530

    Copyright changes

--- src/main/java/com/hcl/appscan/jenkins/plugin/auth/JenkinsAuthenticationProvider.java
+++ src/main/java/com/hcl/appscan/jenkins/plugin/auth/JenkinsAuthenticationProvider.java
@@ -1,6 +1,6 @@
 /**
  * @ Copyright IBM Corporation 2016.
- * @ Copyright HCL Technologies Ltd. 2017, 2019.
+ * @ Copyright HCL Technologies Ltd. 2017, 2019, 2022.
  * LICENSE: Apache License, Version 2.0 https://www.apache.org/licenses/LICENSE-2.0
  */
 
