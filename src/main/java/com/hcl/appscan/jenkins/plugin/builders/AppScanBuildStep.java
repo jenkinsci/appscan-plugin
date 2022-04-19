13ae7e32448924fb8d8406b89972380b6aa106b8
Vishal Chaudhary <102226698+vishalhcl-5960@users.noreply.github.com>
2022-04-19 10:43:30 +0530

    Update AppScanBuildStep.java

--- src/main/java/com/hcl/appscan/jenkins/plugin/builders/AppScanBuildStep.java
+++ src/main/java/com/hcl/appscan/jenkins/plugin/builders/AppScanBuildStep.java
@@ -210,17 +210,17 @@ public class AppScanBuildStep extends Builder implements SimpleBuildStep, Serial
     public DescriptorImpl getDescriptor() {
         return (DescriptorImpl)super.getDescriptor();
     }
-	
-    @Override
-    public boolean perform(AbstractBuild<?,?> build, Launcher launcher, BuildListener listener) throws IOException, InterruptedException {
-	   perform((Run<?,?>)build, launcher, listener);
-	 return true;
-    }
     
     @Override
-    public void perform(@Nonnull Run<?, ?> run, @Nonnull FilePath workspace, @Nonnull Launcher launcher, @Nonnull TaskListener listener) throws InterruptedException, IOException {
-	   perform(run, launcher, listener);
+    public boolean perform(AbstractBuild<?,?> build, Launcher launcher, BuildListener listener) throws IOException, InterruptedException {
+	perform((Run<?,?>)build, launcher, listener);
+	        return true;
     }
+    
+        @Override
+        public void perform(@Nonnull Run<?, ?> run, @Nonnull FilePath workspace, @Nonnull Launcher launcher, @Nonnull TaskListener listener) throws InterruptedException, IOException {
+	        perform(run, launcher, listener);
+        }
     
     @Override
     public BuildStepMonitor getRequiredMonitorService() {
