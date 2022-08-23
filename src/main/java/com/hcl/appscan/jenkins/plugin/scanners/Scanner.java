f3f3d080bc3b346359f878c9fa9c381930a60c8b
Vishal Chaudhary <102226698+vishalhcl-5960@users.noreply.github.com>
2022-08-23 18:07:44 +0530

    Update Scanner.java

--- src/main/java/com/hcl/appscan/jenkins/plugin/scanners/Scanner.java
+++ src/main/java/com/hcl/appscan/jenkins/plugin/scanners/Scanner.java
@@ -47,7 +47,7 @@ public abstract class Scanner extends AbstractDescribableImpl<Scanner> implement
 		Pattern pattern = Pattern.compile("^(\\\\|/|[a-zA-Z]:\\\\)");
 
 		//If the path is not absolute, make it relative to the workspace
-		if(!(path.equals("")) && ((!(path.startsWith("/"))) && (!(path.startsWith("\\"))) && (!pattern.matcher(path).find()))){
+		if(((!(path.startsWith("/"))) && (!pattern.matcher(path).find()))){
 			String targetPath = "${WORKSPACE}" + "//" + path ;
 			targetPath = Util.replaceMacro(targetPath, resolver);
 			return targetPath;
