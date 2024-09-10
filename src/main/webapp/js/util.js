8cf0893922087ea080fde7ba6f362bb50b11e820
Zbynek Konecny <zbynek1729@gmail.com>
2024-09-10 22:05:28 +0200

    Make dependency on YUI optional (#226)

--- src/main/webapp/js/util.js
+++ src/main/webapp/js/util.js
@@ -88,20 +88,22 @@ function aseWaitClicked(e) {
 	}
 }
 
-/*
-   * Overridable method called before autocomplete container is loaded with result data.
-   * This method is overridden to dynamically change the autocomplete result list size
-   * @param sQuery {String} Original request.
-   * @param oResponse {Object} Response object.
-   * @param oPayload {MIXED} (optional) Additional argument(s)
-   * @return {Boolean} Return true to continue loading data, false to cancel.
-
-*/
-YAHOO.widget.AutoComplete.prototype.doBeforeLoadData = function (sQuery, oResponse, oPayload) {
-     if (oResponse.results.length != 0) {
-          YAHOO.widget.AutoComplete.prototype.maxResultsDisplayed = oResponse.results.length;
-     }
-     return true;
+if (window.YAHOO) {
+  /*
+     * Fix for the YUI-based autocomplete field, not needed since Jenkins 2.473.
+     * Overridable method called before autocomplete container is loaded with result data.
+     * This method is overridden to dynamically change the autocomplete result list size
+     * @param sQuery {String} Original request.
+     * @param oResponse {Object} Response object.
+     * @param oPayload {MIXED} (optional) Additional argument(s)
+     * @return {Boolean} Return true to continue loading data, false to cancel.
+  */
+  YAHOO.widget.AutoComplete.prototype.doBeforeLoadData = function (sQuery, oResponse, oPayload) {
+       if (oResponse.results.length != 0) {
+            YAHOO.widget.AutoComplete.prototype.maxResultsDisplayed = oResponse.results.length;
+       }
+       return true;
+  }
 }
 
 function resetFields(credentialElement) {
