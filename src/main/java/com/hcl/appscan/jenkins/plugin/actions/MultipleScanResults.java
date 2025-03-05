23114c1c1b4629c67ce898f367d2c101e0d9db90
Vishal Chaudhary <102226698+vishalhcl-5960@users.noreply.github.com>
2025-03-05 19:02:42 +0530

    Added copyright years

--- src/main/java/com/hcl/appscan/jenkins/plugin/actions/MultipleScanResults.java
+++ src/main/java/com/hcl/appscan/jenkins/plugin/actions/MultipleScanResults.java
@@ -1,3 +1,8 @@
+/**
+ * © Copyright HCL Technologies Ltd. 2025.
+ * LICENSE: Apache License, Version 2.0 https://www.apache.org/licenses/LICENSE-2.0
+ */
+
 package com.hcl.appscan.jenkins.plugin.actions;
 
 import com.hcl.appscan.sdk.results.IResultsProvider;
--- src/main/java/com/hcl/appscan/jenkins/plugin/actions/ScanResults.java
+++ src/main/java/com/hcl/appscan/jenkins/plugin/actions/ScanResults.java
@@ -1,6 +1,6 @@
 /**
  * © Copyright IBM Corporation 2016.
- * @ Copyright HCL Technologies Ltd. 2019, 2024.
+ * @ Copyright HCL Technologies Ltd. 2019, 2024, 2025.
  * LICENSE: Apache License, Version 2.0 https://www.apache.org/licenses/LICENSE-2.0
  */
 
