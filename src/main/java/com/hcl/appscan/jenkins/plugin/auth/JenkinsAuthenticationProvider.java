3ceb2567fff187430729889840829e880e7fcc09
Vishal Chaudhary <102226698+vishalhcl-5960@users.noreply.github.com>
2026-06-04 18:32:31 +0530

    Fix casing updates

--- src/main/java/com/hcl/appscan/jenkins/plugin/auth/JenkinsAuthenticationProvider.java
+++ src/main/java/com/hcl/appscan/jenkins/plugin/auth/JenkinsAuthenticationProvider.java
@@ -137,6 +137,6 @@ public class JenkinsAuthenticationProvider implements IAuthenticationProvider, S
 
     	@Override
     	public boolean getacceptInvalidCerts() {
-        	return m_credentials.getacceptInvalidCerts();
+        	return m_credentials.getAcceptInvalidCerts();
     	}
 }
